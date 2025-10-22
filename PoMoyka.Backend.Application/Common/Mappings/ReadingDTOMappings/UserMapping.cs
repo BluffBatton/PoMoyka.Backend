404f4c0e66c6cbd3d8f0ade7cee478db1a7b8d05
Oleksandr Parkhomenko <havchik17@gmail.com>
2025-10-22 18:10:37 +0300

    Added UserController with GetUser method

---
+++ PoMoyka.Backend.Application/Common/Mappings/ReadingDTOMappings/UserMapping.cs
@@ -0,0 +1,13 @@
+﻿using PoMoyka.Backend.Contracts.DTOs.ReadingDTOs;
+using PoMoyka.Backend.Domain.Entities;
+
+namespace PoMoyka.Backend.Application.Common.Mappings.ReadingDTOMappings
+{
+    public class UserMapping : AutoMapper.Profile
+    {
+        public UserMapping()
+        {
+            CreateMap<User, UserDto>();
+        }
+    }
+}
